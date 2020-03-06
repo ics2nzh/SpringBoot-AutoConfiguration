2ad98509faef375a192b767f645fd3c8dc1383ec
oxygen1997 <chzeyi@126.com>
2020-03-06 14:47:22 +0800

    update.

--- src/main/java/com/zy/demo/autoconfiguration/util/HttpClientUtils.java
+++ src/main/java/com/zy/demo/autoconfiguration/util/HttpClientUtils.java
@@ -1,6 +1,8 @@
 package com.zy.demo.autoconfiguration.util;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +16,20 @@ public class  HttpClientUtils {
     @Autowired
     private  HttpClient httpClient;
 
+    private CloseableHttpResponse response = null;
+
     public  String execute(String key) throws IOException {
-        String url = GetUrlUtil.get(key);
-        if ("fail".equalsIgnoreCase(url)){
-            return "请求的URL不存在...";
+        String responseEntity = null;
+        try{
+            String url = GetUrlUtil.get(key);
+            if ("fail".equalsIgnoreCase(url)){
+                return "请求的URL不存在...";
+            }
+            response  = (CloseableHttpResponse) httpClient.execute(new HttpGet(url));
+            responseEntity = EntityUtils.toString(response.getEntity(),"UTF-8");
+        }finally {
+            response.close();
         }
-        String s = EntityUtils.toString(httpClient.execute(new HttpGet(url)).getEntity(),"UTF-8");
-        return s;
+        return responseEntity;
     }
 }
